6793e1a73f1fe45d5b8e60ce324989d640a8e767
khang-81 <2251061806@e.tlu.edu.vn>
2025-05-01 01:28:51 +0700

    calculator

--- Project_cal/app/src/main/java/com/example/project_cal/MainActivity.java
+++ Project_cal/app/src/main/java/com/example/project_cal/MainActivity.java
@@ -1,24 +1,55 @@
 package com.example.project_cal;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    EditText edt1, edt2, edt3;
+    Button btncong, btntru, btnnhan, btnchia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        edt1 = findViewById(R.id.edta);
+        edt2 = findViewById(R.id.edtb);
+        edt3 = findViewById(R.id.edtc);
+
+        btncong = findViewById(R.id.btncong);
+        btntru = findViewById(R.id.btntru);
+        btnnhan = findViewById(R.id.btnnhan);
+        btnchia = findViewById(R.id.btnchia);
+
+        btncong.setOnClickListener(v -> {
+            int a = Integer.parseInt("0" + edt1.getText());
+            int b = Integer.parseInt("0" + edt2.getText());
+            edt3.setText("a + b = " + (a + b));
+        });
+
+        btntru.setOnClickListener(v -> {
+            int a = Integer.parseInt("0" + edt1.getText());
+            int b = Integer.parseInt("0" + edt2.getText());
+            edt3.setText("a - b = " + (a - b));
+        });
+
+        btnnhan.setOnClickListener(v -> {
+            int a = Integer.parseInt("0" + edt1.getText());
+            int b = Integer.parseInt("0" + edt2.getText());
+            edt3.setText("a * b = " + (a * b));
+        });
+
+        btnchia.setOnClickListener(v -> {
+            int a = Integer.parseInt("0" + edt1.getText());
+            int b = Integer.parseInt("0" + edt2.getText());
+            if (b == 0) {
+                edt3.setText("B phải khác 0");
+            } else {
+                edt3.setText("a / b = " + (a / b));
+            }
         });
     }
 }
